c1a1eeb3a843016dbbb6a023c77cb33c94fc92a9
RAPHAEL <80759812+C4PISTRANO@users.noreply.github.com>
2023-08-16 19:45:39 -0300

    Add files via upload

---
+++ 2023-08-16/Threads/src/LP3A5/THREADS.java
@@ -0,0 +1,29 @@
+package LP3A5;
+public class THREADS {
+public static void main(String[] args) throws InterruptedException {
+// TODO Auto-generated method stub
+class Counter {
+private int value;
+public void increment() {
+value++;
+}
+public int getValue() {
+return value;
+}
+}
+// ...
+Counter counter = new Counter();
+Runnable task = () -> {
+for (int i = 0; i < 1000; i++) {
+counter.increment();
+}
+};
+Thread thread1 = new Thread(task);
+Thread thread2 = new Thread(task);
+thread1.start();
+thread2.start();
+thread1.join();
+thread2.join();
+System.out.println(counter.getValue()); // resultado esperado é 2000, mas pode variar
+}
+}
---
+++ 2023-08-16/ThreadsCorrigida/src/LP3A5/THREADS.java
@@ -0,0 +1,31 @@
+package LP3A5;
+
+public class THREADS {
+class Counter {
+private int value;
+public synchronized void increment() {
+value++;
+}
+public int getValue() {
+return value;
+}
+}
+Inicialização das duas threads
+thread1.start();
+thread2.start();
+// ...
+Counter counter = new Counter();
+Runnable task = () -> {
+for (int i = 0; i < 1000; i++) {) -
+counter.increment();
+}
+};
+Thread thread1 = new Thread(task);
+Thread thread2 = new Thread(task);
+thread1.start();
+thread2.start();
+thread1.join();
+thread2.join();
+System.out.println(counter.getValue()); // resultado esperado é 2000
+}
+}
